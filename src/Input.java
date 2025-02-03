23fa46a58166e14abea7858470348812a10ef9e9
Emi1iano <150746114+Emi1iano@users.noreply.github.com>
2025-02-03 05:14:39 +0000

    Input

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+        System.out.print("Enter Name: ");
+        name = scanner.nextLine();
+
+        int age;
+        System.out.print("Enter Age: ");
+        age = scanner.nextInt();
+
+        float weight;
+        System.out.print("Enter Weight: ");
+        weight = scanner.nextFloat();
+
+        boolean bool;
+        System.out.print("Enter true Or false If You Smoke: ");
+        bool = scanner.nextBoolean();
+
+        System.out.println(name + " is age " + age + " with weight " + weight + " and " + bool + " to smoking");
+    }
+}
